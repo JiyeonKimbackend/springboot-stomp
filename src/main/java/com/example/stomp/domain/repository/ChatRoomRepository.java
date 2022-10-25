266e555e8d6c7eb340c0f68cfffacf951aa04cb4
JiyeonKimbackend <jiyeon.backend@gmail.com>
2022-10-25 19:38:06 +0900

    [수정] JPARespoitory 상속

--- src/main/java/com/example/stomp/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/example/stomp/domain/repository/ChatRoomRepository.java
@@ -2,7 +2,13 @@ package com.example.stomp.domain.repository;
 
 import com.example.stomp.domain.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT c FROM chat_room c ORDER BY c.chat_room_id DESC")
+    List<ChatRoom> findAllDesc();
 
 }
