440b1517efa24eca4253d9288140953036d9af41
JiyeonKimbackend <jiyeon.backend@gmail.com>
2022-11-03 17:36:37 +0900

    [추가] 채팅내역 가져오기 컨트롤러 추가

--- src/main/java/com/example/stomp/control/RoomController.java
+++ src/main/java/com/example/stomp/control/RoomController.java
@@ -1,19 +1,21 @@
 package com.example.stomp.control;
 
 import com.example.stomp.domain.entity.ChatRoom;
+import com.example.stomp.dto.BufferMap;
+import com.example.stomp.dto.ChatMessageDto;
 import com.example.stomp.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
 public class RoomController {
 
     private final ChatService service;
+
+    private final BufferMap<Long, List<ChatMessageDto>> chatListMap;
 
     //채팅방 목록 조회
     @GetMapping(value = "/rooms")
@@ -52,9 +56,23 @@ public class RoomController {
     //채팅방 조회
     @GetMapping("/room")
     public void getRoom(Long roomId, Model model){
-
         log.info("# get Chat Room, roomID : " + roomId);
 
         model.addAttribute("room", service.findRoomById(roomId));
+    }
+
+    //채팅방의 채팅내역 가져오기
+    @GetMapping("/{roomId}")
+    public ResponseEntity<List<String>> getChats(@PathVariable("roomId") Long roomId, HttpServletRequest request){
+        List<ChatMessageDto> chatMesasge = chatListMap.get(roomId);
+
+        if(chatMesasge != null && chatMesasge.size() > 0){
+            /* Buffer의 내용 save 후 clear */
+            service.saveChat(roomId, chatMesasge, request.getServletContext().getRealPath("/chat"));
+            chatMesasge.clear();
+        }
+        List<String> chats = service.getChats(roomId, request.getServletContext().getRealPath("/chat"));
+
+        return new ResponseEntity<>(chats, HttpStatus.OK);
     }
 }
